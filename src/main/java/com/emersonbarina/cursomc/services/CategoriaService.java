2058355343ef3f517d0e7a0edc564bbc51a11341
Emerson Barina <emersonbarina@gmail.com>
2018-06-26 17:01:15 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/emersonbarina/cursomc/services/CategoriaService.java
+++ src/main/java/com/emersonbarina/cursomc/services/CategoriaService.java
@@ -5,8 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.emersonbarina.cursomc.Repositories.CategoriaRepository;
 import com.emersonbarina.cursomc.domain.Categoria;
+import com.emersonbarina.cursomc.repositories.CategoriaRepository;
 
 @Service
 public class CategoriaService {
