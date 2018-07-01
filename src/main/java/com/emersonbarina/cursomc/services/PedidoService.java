d94a186405e5cdac2357c1fb8d9c0079f71bb97e
Emerson Barina <emersonbarina@gmail.com>
2018-07-01 00:19:57 -0300

    Endpoint /pedidos/{id} disponibilizador

---
+++ src/main/java/com/emersonbarina/cursomc/services/PedidoService.java
@@ -0,0 +1,28 @@
+package com.emersonbarina.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emersonbarina.cursomc.domain.Pedido;
+import com.emersonbarina.cursomc.repositories.PedidoRepository;
+import com.emersonbarina.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	//public Pedido buscar(Integer id) {
+	//	Pedido Obj = repo.findOne(id);
+	//	return obj;
+	//	}
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		}
+}
