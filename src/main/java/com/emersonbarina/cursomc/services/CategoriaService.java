65df6edab6584f8e78c4cbbf14829f89384b80e7
Emerson Barina <emersonbarina@gmail.com>
2018-07-02 00:38:17 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/emersonbarina/cursomc/services/CategoriaService.java
+++ src/main/java/com/emersonbarina/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.emersonbarina.cursomc.domain.Categoria;
+import com.emersonbarina.cursomc.dto.CategoriaDTO;
 import com.emersonbarina.cursomc.repositories.CategoriaRepository;
 import com.emersonbarina.cursomc.services.exceptions.DataIntegretyException;
 import com.emersonbarina.cursomc.services.exceptions.ObjectNotFoundException;
@@ -59,7 +60,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
-		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
