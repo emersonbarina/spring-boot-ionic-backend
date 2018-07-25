75a30cc0f5c844f52fbdaf3cd9cef77f56ec00f2
Emerson Barina <emersonbarina@gmail.com>
2018-07-25 19:54:39 -0300

    Restricao de conteudo: cliente so recupera ele mesmo

--- src/main/java/com/emersonbarina/cursomc/services/ClienteService.java
+++ src/main/java/com/emersonbarina/cursomc/services/ClienteService.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.emersonbarina.cursomc.domain.Cidade;
 import com.emersonbarina.cursomc.domain.Cliente;
 import com.emersonbarina.cursomc.domain.Endereco;
+import com.emersonbarina.cursomc.domain.enuns.Perfil;
 import com.emersonbarina.cursomc.domain.enuns.TipoCliente;
 import com.emersonbarina.cursomc.dto.ClienteDTO;
 import com.emersonbarina.cursomc.dto.ClienteNewDTO;
 import com.emersonbarina.cursomc.repositories.ClienteRepository;
 import com.emersonbarina.cursomc.repositories.EnderecoRepository;
+import com.emersonbarina.cursomc.security.UserSS;
+import com.emersonbarina.cursomc.services.exceptions.AuthorizationException;
 import com.emersonbarina.cursomc.services.exceptions.DataIntegretyException;
 import com.emersonbarina.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -36,6 +39,12 @@ public class ClienteService {
 	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
+		
+		UserSS user = UserService.authenticated();
+		if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
