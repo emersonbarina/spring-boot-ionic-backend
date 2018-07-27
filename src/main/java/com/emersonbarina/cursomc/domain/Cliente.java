ee4affb442ac54edfe8579d2a9d2cbc88db57ae3
Emerson Barina <emersonbarina@gmail.com>
2018-07-27 02:30:09 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/emersonbarina/cursomc/domain/Cliente.java
+++ src/main/java/com/emersonbarina/cursomc/domain/Cliente.java
@@ -51,8 +51,6 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
-	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -172,13 +170,4 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-
 }
