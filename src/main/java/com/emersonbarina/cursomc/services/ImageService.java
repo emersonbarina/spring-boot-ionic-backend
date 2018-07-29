20dfe47e798b77d529a53f64459fba1800470c97
Emerson Barina <emersonbarina@gmail.com>
2018-07-29 19:43:00 -0300

    Bonus: ajustando o tamanho da imagem

--- src/main/java/com/emersonbarina/cursomc/services/ImageService.java
+++ src/main/java/com/emersonbarina/cursomc/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,7 +24,7 @@ public class ImageService {
 		if (!"png".equals(ext) && !"jpg".equals(ext)) {
 			throw new FileException("Somente imagens PNG e JPG são permitidas");
 		}
-		
+
 		try {
 			BufferedImage img = ImageIO.read(uploadedFile.getInputStream());
 			if (!"png".equals(ext)) {
@@ -49,5 +50,18 @@ public class ImageService {
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo");
 		}
+	}
+
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(
+			sourceImg, 
+			(sourceImg.getWidth() / 2) - (min / 2), 
+			(sourceImg.getHeight() / 2) - (min / 2),
+			min, min);
+	}
+	
+	public BufferedImage resize(BufferedImage sourcImg, int size) {
+		return Scalr.resize(sourcImg, Scalr.Method.ULTRA_QUALITY, size);
 	}
 }
